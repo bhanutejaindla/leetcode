a7d4ba5e51bb05291b6e5610a1582e5c76bbe9d7
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-09-23 21:40:03 +0530

    Time: 0 ms (100%), Space: 7.6 MB (10.28%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -5,28 +5,28 @@ public:
         int ans=0;
         while(a<=n)
         {
-          ans+=min(b,n+1)-a;
-          a*=10;
-          b*=10;
+            ans+=min(b,n+1)-a;
+            a=a*10;
+            b=b*10;
         }
         return ans;
     }
     int findKthNumber(int n, int k) {
-       long  curr=1;
-       for(int i=1;i<k;)
-       {
-          int gap=find(curr,curr+1,n);
-          if(i+gap<=k)
-          {
-             curr++;
-             i+=gap;
-          }
-          else
-          {
-            i++;
-            curr=curr*10;
-          }
-       }
-       return curr;
+        int curr=1;
+        for(int i=1;i<k;)
+        {
+            int gap=find(curr,curr+1,n);
+            if(i+gap<=k)
+            {
+                i+=gap;
+                curr++;
+            }
+            else
+            {
+                curr=curr*10;
+                i++;
+            }
+        }
+        return curr;
     }
 };
