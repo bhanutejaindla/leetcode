24c945c7c175bd8677013e9800b678230243fc69
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-10-10 00:02:05 +0530

    Time: 4 ms (18.73%), Space: 7.9 MB (32.94%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty())
+            {
+              st.push(s[i]);
+            }
+            else if(st.top()=='(' && s[i]==')')
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+            
+        }
+        return st.size();
+    }
+};
