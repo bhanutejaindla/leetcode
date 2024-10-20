e038fd746067518aac9ae643949bc4041de6c9d3
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-10-21 00:04:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (29.48%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
+                continue;
+            }
+            if((st.top()=='{' && s[i]=='}') ||(st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']'))
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+        return st.empty()==true;
+    }
+};
