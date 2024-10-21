94f4aee18d3c0c66efc91589e97616241c3a7b07
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2024-10-21 12:28:40 +0530

    Time: 3 ms (82.6%), Space: 12.6 MB (7%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>mp;
+        stack<int>st;
         int n=nums2.size();
-        stack<pair<int,int>>st;
-        st.push({nums2[n-1],-1});
+        st.push(nums2[n-1]);
+        vector<int>ans;
+        ans.push_back(-1);
+        unordered_map<int,int>mp;
         mp[nums2[n-1]]=-1;
+   
         for(int i=n-2;i>=0;i--)
         {
-            if(nums2[i]<st.top().first)
-            {
-                mp[nums2[i]]=st.top().first;
-                st.push({nums2[i],st.top().first});
-            }
-            else
-            {
-                while(!st.empty() && nums2[i]>=st.top().first)
-                {
-                    st.pop();
-                }
-                if(!st.empty())
-                {
-                    mp[nums2[i]]=st.top().first;
-                    st.push({nums2[i],st.top().first});
-                    
-                }
-                else
-                {
-                    st.push({nums2[i],-1});
-                    mp[nums2[i]]=-1;
-                }
-            }
+           while(!st.empty() && nums2[i]>=st.top())
+           {
+            st.pop();
+           }
+           if(!st.empty())
+           {
+            mp[nums2[i]]=st.top();
+            ans.push_back(st.top());
+           }
+           else
+           {
+            mp[nums2[i]]=-1;
+            ans.push_back(-1);
+           }
+           st.push(nums2[i]);
         }
-       vector<int>ans;
-       for(int i=0;i<nums1.size();i++)
-       {
-        ans.push_back(mp[nums1[i]]);
-       }
-        return ans;
+        reverse(ans.begin(),ans.end());
+        vector<int>v;
+        for(int i=0;i<nums1.size();i++)
+        {
+          v.push_back(mp[nums1[i]]);
+        }
+        return v;
     }
 };
