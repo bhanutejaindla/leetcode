10742f64a200b33c70b32820c9452c23ff067782
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2025-01-20 08:58:27 +0530

    Time: 0 ms (100%), Space: 19 MB (6.12%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_set<int>row;
-        unordered_set<int>col;
+        set<int>r,c;
         for(int i=0;i<matrix.size();i++)
         {
-            for(int j=0;j<matrix[i].size();j++)
+            for(int j=0;j<matrix[0].size();j++)
             {
-                if(matrix[i][j]==0)
-                {
-                    row.insert(i);
-                    col.insert(j);
-                }
+                 if(matrix[i][j]==0)
+                 {
+                    r.insert(i);
+                    c.insert(j);
+                 }
             }
         }
         for(int i=0;i<matrix.size();i++)
         {
-            for(int j=0;j<matrix[i].size();j++)
+            if(r.find(i)!=r.end())
             {
-                if(row.find(i)!=row.end() || col.find(j)!=col.end())
-                {
-                    matrix[i][j]=0;
-                }
+              for(int j=0;j<matrix[0].size();j++)
+              {
+                matrix[i][j]=0;
+              }
+            }
+        }
+         for(int j=0;j<matrix[0].size();j++)
+        {
+            if(c.find(j)!=c.end())
+            {
+              for(int i=0;i<matrix.size();i++)
+              {
+                  matrix[i][j]=0;
+              }
             }
         }
         return ;
