c1e4e235f83970efc0f662d3f3ddfb0ba36bded8
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2025-01-22 11:45:41 +0530

    Time: 2 ms (38.47%), Space: 95.2 MB (63.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,25 +4,46 @@ public:
         int n1=nums1.size();
         int n2=nums2.size();
         int n=n1+n2;
-        vector<int>v(n);
+        int cnt=0;
+        int cnt1=n/2;
+        int cnt2=n/2-1;
         int i=0,j=0,k=0;
+        int ele1,ele2;
         while(i<n1 && j<n2)
         {
             if(nums1[i]<=nums2[j])
             {
-                v[k++]=nums1[i++];
+                if(cnt==cnt1) ele1=nums1[i];
+                if(cnt==cnt2) ele2=nums1[i];
+                cnt++;
+                i++;
             }
             else
             {
-                v[k++]=nums2[j++];
+               if(cnt==cnt1) ele1=nums2[j];
+               if(cnt==cnt2) ele2=nums2[j];
+               cnt++;
+               j++;
             }
         }
-        while(i<n1 && k<n) v[k++]=nums1[i++];
-        while(j<n2 && k<n) v[k++]=nums2[j++];
-        if((n1+n2)%2==1)  return (double)v[(n1+n2)/2];
+        while(i<n1)
+        {
+            if(cnt==cnt1) ele1=nums1[i];
+            if(cnt==cnt2) ele2=nums1[i];
+            i++;
+            cnt++;
+        }
+        while(j<n2)
+        {
+            if(cnt==cnt1) ele1=nums2[j];
+            if(cnt==cnt2) ele2=nums2[j];
+            j++;
+            cnt++;
+        }
+        if(n%2==1)  return (double)ele1;
         else
         {
-            return (double)(v[(n1+n2)/2 -1]+v[(n1+n2)/2])/2;
+            return (double)(ele1+ele2)/2;
         }
     }
 };
