00ddbea6ca9f71dc25750c70d09028c80ef6f180
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2025-03-31 19:00:14 +0530

    Time: 0 ms (100%), Space: 10.1 MB (92.54%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>&board,int n)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(board[row][j]=='Q')  return false;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(board[i][col]=='Q')  return false;
+        }
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        {
+            if(board[i][j]=='Q')  return false;
+        }
+        for(int i=row,j=col;i>=0 && j<n;i--,j++)
+        {
+            if(board[i][j]=='Q')  return false;
+        }
+        return true;
+    }
+    void solve(int row,vector<string>&board,vector<vector<string>>&ans,int n)
+    {
+        if(row==n)
+        {
+            ans.push_back(board);
+            return ;
+        }
+        for(int col=0;col<n;col++)
+        {
+            if(isSafe(row,col,board,n))
+            {
+                board[row][col]='Q';
+                solve(row+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        solve(0,board,ans,n);
+        return ans;
+    }
+};
