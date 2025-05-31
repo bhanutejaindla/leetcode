c93f657c4f0b0cd8e30feb89fa072ef2df80ed1f
bhanutejaindla <87014662+bhanutejaindla@users.noreply.github.com>
2025-05-31 09:25:47 +0530

    Time: 4 ms (41.2%), Space: 42.9 MB (14.35%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -1,37 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Pair<TreeNode, Pair<Integer, Integer>>> q = new LinkedList<>();
-        q.offer(new Pair<>(root, new Pair<>(0, 0))); // Pair<TreeNode, (x, y)>
+          Map<Integer,TreeMap<Integer,PriorityQueue<Integer>>> nodes = new TreeMap<>();
+          Queue<Pair<TreeNode,Pair<Integer,Integer>>> todo = new LinkedList<>();
 
-        while (!q.isEmpty()) {
-            Pair<TreeNode, Pair<Integer, Integer>> p = q.poll();
-            TreeNode node = p.getKey();
-            int x = p.getValue().getKey();
-            int y = p.getValue().getValue();
+          todo.offer(new Pair<>(root,new Pair<>(0,0)));
 
-            map
-                .computeIfAbsent(x, k -> new TreeMap<>())
-                .computeIfAbsent(y, k -> new PriorityQueue<>())
-                .offer(node.val);
+          while(!todo.isEmpty())
+          {
+            Pair<TreeNode,Pair<Integer,Integer>> p = todo.poll();
+            TreeNode temp = p.getKey();
+            int x= p.getValue().getKey();
+            int y= p.getValue().getValue();
 
-            if (node.left != null)
-                q.offer(new Pair<>(node.left, new Pair<>(x - 1, y + 1)));
-            if (node.right != null)
-                q.offer(new Pair<>(node.right, new Pair<>(x + 1, y + 1)));
-        }
-
-        List<List<Integer>> result = new ArrayList<>();
-        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            nodes.computeIfAbsent(x, k-> new TreeMap<>())
+                 .computeIfAbsent(y,k-> new PriorityQueue<>())
+                 .offer(temp.val);
+            
+            if(temp.left!=null){
+                todo.add(new Pair<>(temp.left,new Pair<>(x-1,y+1)));
+            }
+            if(temp.right!=null){
+                todo.add(new Pair<>(temp.right,new Pair<>(x+1,y+1)));
+            }
+          }
+          List<List<Integer>>ans = new ArrayList<>();
+          for(TreeMap<Integer, PriorityQueue<Integer>> ys : nodes.values()){
             List<Integer> col = new ArrayList<>();
-            for (PriorityQueue<Integer> pq : ys.values()) {
-                while (!pq.isEmpty()) {
+            for(PriorityQueue<Integer> pq : ys.values()){
+                while(!pq.isEmpty())
+                {
                     col.add(pq.poll());
                 }
             }
-            result.add(col);
-        }
-
-        return result;
+            ans.add(col);
+          }
+          return ans;
     }
 }
