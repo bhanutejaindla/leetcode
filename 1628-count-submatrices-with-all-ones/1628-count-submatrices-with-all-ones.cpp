f967efd01fb4662298bf9ba7fe986fb24fde82a5
BhanuTeja Indla <87014662+bhanutejaindla@users.noreply.github.com>
2025-08-21 14:39:49 +0530

    Time: 15 ms (43.27%), Space: 21.6 MB (16.52%) - LeetHub

--- 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/1628-count-submatrices-with-all-ones.cpp
@@ -3,31 +3,28 @@ public:
     int numSubmat(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-        for(int i=0;i<n;i++)
-        {
-            int cnt=0;
-            for(int j=0;j<m;j++)
-            {
-               if(mat[i][j]==1) cnt++;
-               else
-               {
-                  cnt=0;
-               }
-                mat[i][j]=cnt;
-            }
-        }
         int ans=0;
+        vector<int>h(m,0);
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                int mini=INT_MAX;
-                 for(int k=i;k<n;k++)
-                 {
-                      mini=min(mini,mat[k][j]);
-                      ans+=mini;
-                 }
-            }
+           for(int j=0;j<m;j++) h[j]=mat[i][j]?h[j]+1:0;
+           vector<int>sum(m);
+           stack<int>st;         
+           for(int j=0;j<m;j++)
+           {
+              while(!st.empty() && h[st.top()]>=h[j]) st.pop();
+              if(!st.empty())
+              {
+                 int p=st.top();
+                 sum[j]+=sum[p]+h[j]*(j-p);
+              }
+              else
+              {
+                 sum[j]=h[j]*(j+1);
+              }
+              st.push(j);
+              ans+=sum[j];
+           }
         }
         return ans;
     }
