0f4161e247a0ecaba7efa5f41bf7d4dc9527c025
BhanuTeja Indla <87014662+bhanutejaindla@users.noreply.github.com>
2026-03-24 23:07:34 +0530

    Time: 39 ms (13.65%), Space: 85.7 MB (41.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,70 +1,32 @@
+// C++
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>prev(n,0);
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-            if(st.empty())
-            {
-                st.push(i);
-                prev[i]=0;
-                continue;
-            }
-            while(!st.empty() && heights[st.top()]>=heights[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                prev[i]=0;
-            }
-            if(!st.empty())
-            {
-                prev[i]=st.top()+1;
-            }
+        int n = heights.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        // Nearest Smaller to Left
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            left[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-        for(auto it:prev)
-        {
-            cout<<it<<" ";
-        }
-        cout<<endl;
-        while(!st.empty()) st.pop();
-        vector<int>next(n,n-1);
-        for(int i=n-1;i>=0;i--)
-        {
-            if(st.empty())
-            {
-                st.push(i);
-                next[i]=n-1;
-                continue;
-            }
-            while(!st.empty() && heights[st.top()]>=heights[i])
-            {
-                st.pop();
-            }
-            if(st.empty())
-            {
-                next[i]=n-1;
-            }
-            if(!st.empty())
-            {
-                next[i]=st.top()-1;
-            }
+
+        while (!st.empty()) st.pop();
+
+        // Nearest Smaller to Right
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) st.pop();
+            right[i] = st.empty() ? n : st.top();
             st.push(i);
         }
-         for(auto it:next)
-        {
-            cout<<it<<" ";
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            maxArea = max(maxArea, heights[i] * width);
         }
-        cout<<endl;
-        int maxiarea=0;
-        for(int i=0;i<n;i++)
-        {
-            maxiarea=max(maxiarea,(next[i]-prev[i]+1)*heights[i]);
-        }
-        return maxiarea;
+        return maxArea;
     }
 };
